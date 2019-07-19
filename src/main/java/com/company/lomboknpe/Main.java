e3be79450848945c049205c8e1ffc1c67e2f7166
amseager <amseager@mail.ru>
2019-07-19 13:23:27 +0300

    init

---
+++ src/main/java/com/company/lomboknpe/Main.java
@@ -0,0 +1,10 @@
+package com.company.lomboknpe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SampleEntity sampleEntity = new SampleEntity();
+		sampleEntity.setName("name");
+		System.out.println(sampleEntity);
+	}
+}
---
+++ src/main/java/com/company/lomboknpe/SampleEntity.java
@@ -0,0 +1,9 @@
+package com.company.lomboknpe;
+
+import lombok.Data;
+
+@Data
+public class SampleEntity {
+
+	private String name;
+}
